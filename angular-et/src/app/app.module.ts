bd3bae51762f3341540ffb003acd5378ae615154
Julian Mercado <julianmercadomeza@gmail.com>
2022-06-12 18:15:21 -0400

    Initial Angular project

---
+++ angular-et/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { MediaItemComponent } from './components/media-item/media-item.component';
+import { MediaItemListComponent } from './components/media-item-list/media-item-list.component';
+
+@NgModule({
+  imports: [
+    BrowserModule
+  ],
+  declarations: [
+    AppComponent,
+    MediaItemComponent,
+    MediaItemListComponent
+  ],
+  bootstrap: [
+    AppComponent
+  ]
+})
+
+export class AppModule {}
