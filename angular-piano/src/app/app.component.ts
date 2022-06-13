ff824ba7c03266146dfb4466b4f1c53752128517
Julian Mercado <julianmercadomeza@gmail.com>
2022-06-12 22:20:55 -0400

    add code for angular piano app

---
+++ angular-piano/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Piano App';
+
+  applySound(soundNumber: number) {
+    const audio = new Audio();
+    audio.src = `../assets/sounds/note${soundNumber}.wav`;
+    audio.load()
+    audio.play()
+  }
+
+}
