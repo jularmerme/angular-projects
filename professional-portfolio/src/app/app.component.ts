ae0f5a03156790964ecfdaafbfd0dbdfee6d74d6
Julian Mercado <julianmercadomeza@gmail.com>
2024-05-27 19:32:30 -0400

    Created components for welcome portfolio and contact

--- professional-portfolio/src/app/app.component.ts
+++ professional-portfolio/src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { HomeBannerComponent } from './components/home-banner/home-banner.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [
+    RouterOutlet,
+    ProjectsComponent,
+    HomeBannerComponent,
+    ContactComponent,
+  ],
 })
 export class AppComponent {
-  title = 'professional-portfolio';
+  title = 'Julian Mercado - Professional Profile';
 }
