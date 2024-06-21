54f1d5889681a8922779fb6b1428242805fe0944
Julian Mercado <julianmercadomeza@gmail.com>
2024-06-21 12:17:24 -0400

    Updated skills content

--- professional-portfolio/src/app/app.component.ts
+++ professional-portfolio/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { HomeBannerComponent } from './components/home-banner/home-banner.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
@@ -14,6 +15,7 @@ import { ContactComponent } from './components/contact/contact.component';
     ProjectsComponent,
     HomeBannerComponent,
     ContactComponent,
+    FooterComponent,
   ],
 })
 export class AppComponent {
